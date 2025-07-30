a7d6e755f32e3abf6ecc966f495e597e2a09deda
Berkan <ozmberkan@gmail.com>
2025-07-30 17:27:22 +0300

    feat: add home route with welcome message

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
 
+app.get("/", (req, res) => {
+  res.send(`
+    <div style="font-family: sans-serif; text-align: center; min-height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+      <h2 style="color: #444;"><span style="color: #ff4d6d;font-size: 64px;line-height:1; letter-spacing:-4px; font-family: sans">Biletinle</span> </h2>
+        <h2 style="color: #444;">✅SUNUCU✅</h2>
+    </div>
+  `);
+});
+
 app.listen(process.env.PORT, () => {
   console.log("✅ - Sunucu Aktif! --- PORT: " + process.env.PORT);
 });
